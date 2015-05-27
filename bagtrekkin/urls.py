19efb8bbbee56305346d9a96c1c4e45da1b5ed42
Hugo Rodde <rodde.hugo@gmail.com>
2015-05-27 15:40:15 -0300

    URLS

--- bagtrekkin/urls.py
+++ bagtrekkin/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', "bagtrekkin.views.inicio"),
-    url(r"^media/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
+    # url(r"^media/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
     url(r'^bagtrekkin/cadastro/$', "bagtrekkin.views.cadastro"),
     url(r'^bagtrekkin/login/', "django.contrib.auth.views.login", {"template_name": "login.html"}),
     url(r'^index/$', "bagtrekkin.views.index"),
