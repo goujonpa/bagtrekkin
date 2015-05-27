6a9128c3c0768411afa658d5a0ec2e1308dcd7a7
Hugo Rodde <rodde.hugo@gmail.com>
2015-05-27 16:17:37 -0300

    Migrations initial

--- bagtrekkin/migrations/0001_initial.py
+++ bagtrekkin/migrations/0001_initial.py
@@ -4,10 +4,19 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def load_stores_from_sql():
+    from coffeehouse.settings import BASE_DIR
+    import os
+    sql_statements = open(os.path.join(BASE_DIR, 'psql.sql'), 'r').read()
+    return sql_statements
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('stores', '0001_initial'),
     ]
 
     operations = [
+        migrations.RunSQL(load_stores_from_sql()),
     ]
