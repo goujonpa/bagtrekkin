c90cd454581056c2492f20244cefe73f467993d0
Hugo Rodde <rodde.hugo@gmail.com>
2015-06-03 20:19:16 -0300

    modif

--- bagtrekkin/urls.py
+++ bagtrekkin/urls.py
@@ -13,13 +13,9 @@ urlpatterns = patterns(
     url(r'^api/', include(materials_resource.urls)),
 
     url(r'^$', bt_views.index, name='bt_index'),
-
-    url(r'login\.html', auth_views.login, {'template_name': 'login.jade'}, name='bt_login'),
-    url(r'logout\.html', auth_views.logout_then_login, name='bt_logout'),
-    url(r'signup\.html', bt_views.signup, name='bt_signup'),
     url(r'profile\.html', bt_views.profile, name='bt_profile'),
     url(r'actions\.html', bt_views.actions, name='bt_actions'),
 
-    # url(r'^fligths/', bt_views.fligths, name='bt_flights'),
-    # url(r'^fligths/(?P<airline>.*\w+)/', bt_views.checkin, name='bt_airline'),
+    url(r'^fligths/', bt_views.fligths, name='bt_flights'),
+    url(r'^fligths/(?P<airline>.*\w+)/', bt_views.checkin, name='bt_airline'),
 )
